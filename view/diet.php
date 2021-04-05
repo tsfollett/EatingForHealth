bad6019e1e09e5514eb7e7df938e161cc2f50884
mark390 <mark.ross@ampsight.com>
2021-04-04 21:40:44 -0400

    navigation changes

--- view/diet.php
+++ view/diet.php
@@ -1,39 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Eating 4 Health</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="logo1.png">
-  <link rel="stylesheet" type="text/css" href="main.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  
-</head>
-<body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <a class="navbar-brand" href="#">
-         <img src="logo2.png" alt="Logo" style="width:100px;">
-       </a>
-    <div class="topnav" id="myTopnav">
-        <a href="home.html" class="active">Home</a>
-        <a href="diet.html">The Diet</a>
-        <div class="dropdown">
-          <button class="dropbtn">Meal Support
-            <i class="fa fa-caret-down"></i>
-          </button>
-          <div class="dropdown-content">
-            <a href="substitutes.html">Substitutes</a>
-            <a href="recipes.html">Recipes</a>
-            <a href="weekplan.html">Weekly Planner</a>
-          </div>
-        </div>
-        <a href="resources.html">Resources</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-      </div>
-  
+<?php include('header.php') ?>
+
 <div class="container text-center">    
   <h3>What We Do</h3><br>
   <div class="row">
