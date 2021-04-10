e125bcb2fcf64e64dc078d2390d1d061cdcf005b
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-04-10 11:31:40 -0600

    finished diet page

--- view/diet.php
+++ view/diet.php
@@ -3,59 +3,61 @@
 <h1 class="text-center">The Diet</h1>
 
 <h4><u>Phase 1</u></h4>
-<p>For 30, you will avoid the foods listed on your food sensitivity test AND the following foods&colon;</p>
-<ul>
-  <li>Dairy &#40;no milk, cheese, yogurt, ghee, butter, margarine, shortening, goat&rsquo;s milk&#41;</li>
-  <li>Eggs</li>
-  <li>Gluten &#40;no wheat, oats, rye, barley which are typically found in breads, pasta, cereals&#41;</li>
-  <li>Tomatoes, tomato sauce, or anything with tomatoes</li>
-  <li>Dehydrated fruit (usually has preservatives and sugar)</li>
-  <li>Rice</li>
-  <li>Corn</li>
-  <li>Potatoes</li>
-  <li>Alcohol</li>
-  <li>Coffee, black tea, soda &#40;all caffeine&#41;</li>
-  <li>Fruit juices</li>
-  <li>Iodized salt &#40;table&#41;</li>
-  <li>Sugar, natural & artificial sweeteners &#40;including brown sugar, 
-      honey, stevia, Splenda, agave, monk fruit, erythritol&#41;</li>
-  <li>Soy or products containing soy &#40;including Tofu&#41;</li>
-  <li>Beef, pork, shellfish, cold cuts, bacon, hot dogs, canned meat, sausage, game meat</li>
-  <li>Lentils</li>
-  <li>Chocolate</li>
-  <li>Dates</li> 
-  <li>Peanuts</li>
-</ul>
-<br>
+<p>For 30 days, you will avoid the foods listed on your food sensitivity test AND the following foods&colon;</p>
+<div class="row">
+  <div class="col">
+    <ul>
+      <li>Dairy &#40;no milk, cheese, yogurt, ghee, butter, margarine, shortening, goat&rsquo;s milk&#41;</li>
+      <li>Eggs</li>
+      <li>Gluten &#40;no wheat, oats, rye, barley which are typically found in breads, pasta, cereals&#41;</li>
+      <li>Tomatoes, tomato sauce, or anything with tomatoes</li>
+      <li>Dehydrated fruit (usually has preservatives and sugar)</li>
+      <li>Rice</li>
+      <li>Corn</li>
+      <li>Potatoes</li>
+      <li>Alcohol</li>
+      <li>Coffee, black tea, soda &#40;all caffeine&#41;</li>
+    </ul>
+  </div>
+  <div class="col">
+    <ul>
+      <li>Fruit juices</li>
+      <li>Iodized salt &#40;table&#41;</li>
+      <li>Sugar, natural & artificial sweeteners &#40;including brown sugar, 
+          honey, stevia, Splenda, agave, monk fruit, erythritol&#41;</li>
+      <li>Soy or products containing soy &#40;including Tofu&#41;</li>
+      <li>Beef, pork, shellfish, cold cuts, bacon, hot dogs, canned meat, sausage, game meat</li>
+      <li>Lentils</li>
+      <li>Chocolate</li>
+      <li>Dates</li> 
+      <li>Peanuts</li>
+    </ul>
+  </div>
+</div>
 
 <div class="row">
-  <div class="col-sm-7">
-    <p>For support, use the resources on this site&colon;</p>
-    <div class="row">
-      <div class="col-6">
-        <ul>
-          <li><a href="substitutes.php">Substitutes</a></li>
-          <li><a href="recipes.php">Recipes</a></li>
-        </ul>
-      </div>
-      <div class="col-6">
-        <ul>
-          <li><a href="weekplan.php">Weekly Planner</a></li>
-          <li><a href="resources.php">Resources</a></li>
-        </ul>
-      </div>
-    </div>
-    <p>Download Phase 1 List &#40;PDF&#41;</p>
+  <div class="col-md-5 align-middle"><br>
+    <p><u>For support, use the resources on this site&colon;</u></p>
+      <ul>
+        <li><a href="substitutes.php">Substitutes</a></li>
+        <li><a href="recipes.php">Recipes</a></li>
+        <li><a href="weekplan.php">Weekly Planner</a></li>
+        <li><a href="resources.php">Resources</a></li>
+      </ul>
+    <br>
+    <p><a href="resource_files/Anti-inflammatoryEliminationDiet.pdf" target="_blank">Download Phase 1 List</a> &#40;PDF&#41;</p>
   </div>
 
 <!--This is not showing up...need to work on-->
-  <div class="col-sm-5">
-    <div class="embed-responsive">
+  <div class="col-md-7">
+  <b>Take a tour of the site&colon;</b>
+    <div class="embed-responsive embed-responsive-16by9">
       <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
     </div>
   </div>
 </div>
 
+<br><br>
 <h4><u>Phase 2</u></h4>
 <p>In your next appointment, you will make a plan with Dr. Shannon to reintroduce
   foods not on your food sensitivity list.</p>
