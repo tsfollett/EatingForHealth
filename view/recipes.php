dd2053048ba546a16057a21471d120fd6233b16d
System Administrator <root@Marks-Mac-mini.local>
2021-04-10 14:23:06 -0400

    recipe updates

--- view/recipes.php
+++ view/recipes.php
@@ -1,25 +1,49 @@
 <?php include('header.php')?>
-  
-<div class="container text-center">    
-  <h3>Please use wireframe in Google Drive folder</h3><br>
-  <div class="row">
-    <div class="col-sm-4">
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-      <p>Current Project</p>
-    </div>
-    <div class="col-sm-4"> 
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-      <p>Project 2</p>    
-    </div>
-    <div class="col-sm-4">
-      <div class="well">
-       <p>Some text..</p>
-      </div>
-      <div class="well">
-       <p>Some text..</p>
-      </div>
-    </div>
-  </div>
-</div><br>
+<form>
+<section id="list" class="list">
+    <header class="list row list header">
+    <form action="." method="post" id="list" class="list_header_select">
+    <input type="hidden" name="action" value="list_meal_select">
+    <select name="mealid" class="form-select" aria-label="Select Meal" required>
+    <option selected>View All Recipes</option>
+    <option value="1">Breakfast</option>
+    <option value="2">Lunch</option>
+    <option value="3">Dinner</option>
+    <option value="4">Snack</option>
+    <option value="5">Dessert</option>
+    <option value="6">Drink</option>
+    </select>
+    <label>Avoid</label>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+  <label class="form-check-label" for="inlineCheckbox1">Gluten</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">Dairy</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+  <label class="form-check-label" for="inlineCheckbox1">Sugar</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">Coconut</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+  <label class="form-check-label" for="inlineCheckbox1">Shellfish</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">Eggs</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+  <label class="form-check-label" for="inlineCheckbox2">Nuts</label>
+</div>
+<button class="add-button bold" id="user">Search</button><br>
+    </form>
+    </header>
 
-<?php include('footer.php')?>
+<?php include('view/footer.php'); ?>
