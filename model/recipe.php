1bad7cc11fe3613f6e7e738deedf2c1c5d9764b7
mark390 <55713492+mark390@users.noreply.github.com>
2021-04-11 14:06:17 -0400

    More updates

--- model/recipe.php
+++ model/recipe.php
@@ -12,12 +12,26 @@ function get_recipes() {
 
 function get_recipe($id) {
     global $db;
-    $query = 'SELECT * FROM `recipe` WHERE recipe_id = :id'; 
+    $query = 'SELECT recipe_name, description FROM `recipe` WHERE recipe_id = :id'; 
     $statement = $db->prepare($query);
     $statement->bindValue(':id', $id);
     $statement->execute();
-    $recipes = $statement->fetchAll();
+    $recipes = $statement->fetch();
     $statement->closeCursor();
     return $recipes;
+}
+
+function get_ingredients($id) {
+    global $db;
+    $query = 'SELECT Q.measurement_qty, M.measurement, I.ingredient_name FROM recipe_ingredients X
+    LEFT JOIN meaurement_qty Q on X.measurement_qty_id = Q.measurement_qty_id
+    LEFT JOIN measurement M on X.measurement_id = M.measurement_id
+    LEFT JOIN ingredients I on X.ingredient_id = I.ingredient_id WHERE X.recipe_id = :id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+    $ingredients = $statement->fetchAll();
+    $statement->closeCursor();
+    return $ingredients;
 }
 
--- view/single_recipe.php
+++ view/single_recipe.php
@@ -1,5 +1,24 @@
 <?php include('header.php'); ?>
+<?php require('../model/recipe.php'); ?>
+<?php require('../model/database.php') ?>
 
-<?php $id = filter_input(INPUT_GET, 'rid', FILTER_VALIDATE_INT); ?>
+<?php $id = filter_input(INPUT_GET, 'rid', FILTER_VALIDATE_INT);
+$ingredients = get_ingredients($id);
+$recipe = get_recipe($id); ?>
 
+<main>
+<h1 style="text-align: center;"><?php echo $recipe['recipe_name']; ?></h1>
+<div id="column1" style="float:left; width:23%; padding: 0 10px;">
+<h2 style="text-align: center;">Ingredients</h2>
+<ul>
+<?php foreach($ingredients as $i) : ?> 
+    <li><?php $formatted = $i["measurement_qty"]." ".$i["measurement"]." ".$i["ingredient_name"]; echo $formatted; ?></li>
+<?php endforeach; ?>
+</ul>
+</div>
+<div id="column2" style="float:left; width:auto; padding: 10 20px;">
+<h2 style="text-align: center;">Recipe Steps</h2>
+<?php echo $recipe['description']; ?>
+</div>
+</main><br><br><br>
 <?php include('footer.php'); ?>
