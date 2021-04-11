56666d99c76412438edb67c69950da4b0adc6dcc
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-04-10 21:12:57 -0600

    update shopping resource

--- view/resource-shopping.php
+++ view/resource-shopping.php
@@ -1,6 +1,106 @@
 <?php include('header.php')?>
 
-<h1 class="text-center">Shopping&colon; Where do I buy...?</h1>
+<h1 class="text-center">Shopping&colon; Where do I buy...?</h1><br>
 
+<p class="text-center">Now that you are eating differently and probably purchasing some new ingredients, here are some ideas of where 
+    to pick up different items.</p><br>
+
+<div class="row">
+    <div class="col-md-3 text-center">
+        <img src="images/resources-pg/Costco-Logo.png" alt="Costco Logo" width="200px">
+    </div>
+    <div class="col-md-9">
+        <h6>Items at Costco&colon;</h6>
+        <div class="row">
+            <div class="col">
+                <ul>
+                    <li>Organic almond butter</li>
+                    <li>Nuts &#40;almonds, walnuts, pecans&#41;</li>
+                    <li>Organic canned chicken</li>
+                    <li>Fresh and frozen vegetables &amp; fruits</li>
+                    <li>Coconut oil</li>
+                    <li>Olive oil</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul>
+                    <li>Avocado oil</li>
+                    <li>Zucchini noodles &#40;frozen section&#41;</li>
+                    <li>Riced cauliflower &#40;frozen section&#41;</li>
+                    <li>Canned tuna</li>
+                    <li>Hemp seeds</li>
+                    <li>Chia seeds</li>
+                </ul>
+            </div>
+        </div>
+        <p>Download <a href="resource_files/CostcoList.pdf" target="_blank">Dr. Shannon&rsquo;s Costco shopping list &#40;PDF&#41;</a></p>
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="col-md-3 text-center">
+        <img src="images/resources-pg/TraderJoesLogo.png" alt="Trader Joe's Logo" width="200px">
+    </div>
+    <div class="col-md-9">
+        <h6>Items at Trader Joe&rsquo;s&colon;</h6>
+        <div class="row">
+            <div class="col">
+                <ul>
+                    <li>Organic hummus &#40;garlic or spicy hummus&#41;</li>
+                    <li>Nuts &#40;almonds, walnuts, pecans, macademia&#41;</li>
+                    <li>Cold cuts&colon; sliced turkey withour preservatives</li>
+                    <li>Fresh and frozen vegetables &amp; fruits</li>
+                    <li>Cassava flour chips</li>
+                    <li>Jicama wraps &#40;fridge section&#41;</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul>
+                    <li>Broccoli slaw &#40;fridge section&#41;</li>
+                    <li>Riced cauliflower &#40;frozen section&#41;</li>
+                    <li>Sunflower butter</li>
+                    <li>Almond butter</li>
+                    <li>Coleslaw &#40;fridge section&#41;</li>
+                </ul>
+            </div>
+        </div>
+        <p>Download <a href="resource_files/TraderJoesList.pdf" target="_blank">Dr. Shannon&rsquo;s Trader Joe&rsquo;s shopping list &#40;PDF&#41;</a></p>
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="col-md-3 text-center">
+        <img src="images/resources-pg/natural-grocers.jpg" alt="Natural Grocers Logo" width="200px">
+    </div>
+    <div class="col-md-9">
+        <h6>Items at Natural Grocers&colon;</h6>
+        <div class="row">
+            <div class="col">
+                <ul>
+                    <li>Organic hummus</li>
+                    <li>Nuts &#40;almonds, walnuts, cashews, macademia&#41;</li>
+                    <li>Nutritional Yeast</li>
+                    <li>Uncured Bacon &#40;if pork allowed&#41;</li>
+                    <li>Flours&colon; coconut, buckwheat, garbanzo, brown rice</li>
+                    <li>Veganaise</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul>
+                    <li>Dairy-free items &#40;check ingredients&#41;</li>
+                    <li>Pea Protein</li>
+                    <li>Sunflower butter</li>
+                    <li>Almond butter</li>
+                    <li>Cream of Buckwheat</li>
+                    <li>Organic spices</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<!--And then there's Sprouts (Miyokos, Kite Hill, hummus, Lily's, no sugar added So Delicious coconut ice cream)...
+And Sam's Club (Apple Cider Vinegar, Almond Flour, uncured turkey bacon, coconut oil, avocado oil, rotisserie chicken for those on the go). 
+And the natural section in King Soopers can carry a lot if you have a big one.-->
 
 <?php include('footer.php')?>
